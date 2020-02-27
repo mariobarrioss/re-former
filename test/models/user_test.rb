0e8b2dd02b3e87de6dd7765ccab66903ec44b3af
Mario Barrios <mariobarriossalazar@outlook.com>
2020-02-27 15:40:51 -0600

    Added test suite for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "example", email: "example@example.com", password: "foobar")
+  end
+  
+  test "Should have a name" do
+    @user.username = ' '
+    assert_not @user.valid? 
+  end
+
+  test "Should have a non empty email" do
+    @user.email = ' '
+    assert_not @user.valid?
+  end
+
+  test "Should have a valid email" do
+    @user.email = "ExaMplE@ExAmPlE..co.in"
+    assert_not @user.valid?
+  end
+
+  test "Length of email should be a maximum of 255" do
+    @user.email = "A" * 255 +"@example.com"
+    assert_not @user.valid?
+  end
+
+  test "Password should be non-empty" do
+    @user.password = ' '
+    assert_not @user.valid?
+  end
+  
 end
