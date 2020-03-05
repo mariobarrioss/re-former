bc321b4c22434ae1bf3df382173089fbb45bcd3c
rsundar <rohan.s.1987@gmail.com>
2020-03-05 23:51:51 +0530

    error correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,23 @@ class UsersController < ApplicationController
     def edit
         @user = User.find_by(params[:id])
     end
-    
+
     def update
         @user = User.find_by(params[:id])
         if @user.save
-            @user.Update(user_params)
-            redirect_to new_users_path
+            @user.update(user_params)
+            redirect_to users_path
         else
             render :edit
         end
     end
 
     def create
-        @user = User.new(users_params)
+        #Manual form entry
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        #@user = User.new(users_params)
         if @user.save
-            redirect_to new_users_path
+            redirect_to new_user_path
         else
             render :new
         end
@@ -28,7 +30,7 @@ class UsersController < ApplicationController
 
     private
     
-    def users_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+        def users_params
+            params.require(:user).permit(:username, :email, :password)
+        end
 end
